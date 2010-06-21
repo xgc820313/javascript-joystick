64eb04910c473282fb28170b783462e0860f4459
cwoffenden <cwoffenden@139a8f3a-00d0-11de-9d1d-173cc1d06e3c>
2010-06-21 21:46:33 +0000

    Renamed Java example page. Updated JavaScript with additions for Flash (static methods to manage instances).

--- examples/javascript/joystick.js
+++ examples/javascript/joystick.js
@@ -14,6 +14,95 @@ function Joystick() {
 Joystick.CENTER = 32768;
 
 /**
+ * Reads the controller's main stick x-axis.
+ * 
+ * @return a value between 0 and 65535 (with 0 being extreme left and 65535 extreme right)
+ */
+Joystick.prototype.getX = function() {
+	return (this.ctl) ? this.ctl.x : Joystick.CENTER;
+};
+
+/**
+ * Reads the controller's main stick y-axis.
+ * 
+ * @return a value between 0 and 65535 (with 0 being extreme top and 65535 extreme bottom)
+ */
+Joystick.prototype.getY = function() {
+	return (this.ctl) ? this.ctl.y : Joystick.CENTER;
+};
+
+/**
+ * Reads the controller's designated A button.
+ * 
+ * @return {@code true} if the button is pressed
+ */
+Joystick.prototype.getA = function() {
+	return (this.ctl) ? this.ctl.a : false;
+};
+
+/**
+ * Reads the controller's designated B button.
+ * 
+ * @return {@code true} if the button is pressed
+ */
+Joystick.prototype.getB = function() {
+	return (this.ctl) ? this.ctl.b : false;
+};
+
+/**
+ * Reads the state of all the controller's non-directional buttons.
+ * 
+ * @return the buttons represented as bits in an integer
+ */
+Joystick.prototype.getButtons = function() {
+	return (this.ctl) ? this.ctl.buttons : 0;
+};
+
+/**
+ * Reads multiple controller properties.
+ * 
+ * @param prop array of property names
+ * @param off offset into prop where the names start
+ * @param len number of names to read
+ * @return an array of joystick properties
+ */
+Joystick.prototype.read = function(prop, off, len) {
+	switch (arguments.length) {
+	case 0:
+		return null;
+	case 1:
+		off = 0;
+	case 2:
+		len = prop.length - off;
+	}
+	var ret = [];
+	for (var n = 0; n < len; n++) {
+		switch (prop[off + n]) {
+		case "x":
+			ret.push(this.getX());
+			break;
+		case "y":
+			ret.push(this.getY());
+			break;
+		case "a":
+			ret.push(this.getA());
+			break;
+		case "b":
+			ret.push(this.getB());
+			break;
+		case "buttons":
+			ret.push(this.getButtons());
+			break;
+		default:
+			ret.push(0);
+		}
+	}
+	return ret;
+};
+
+//****************************** Static Methods *****************************/
+
+/**
  * Creates the browser dependent plug-in or ActiveX counterpart.
  */
 Joystick.createPlugin = function() {
@@ -89,37 +178,48 @@ Joystick.createPlugin = function() {
 };
 
 /**
- * Returns the controller's main stick x-axis.
- * 
- * @return a value between 0 and 65535 (with 0 being extreme left and 65535 extreme right)
+ * Contains the joystick wrapper instances used by register.
  */
-Joystick.prototype.getX = function() {
-	if (this.ctl) {
-		return this.ctl.x;
+Joystick.registered = [];
+
+/**
+ * Helper method for Flash (or other plug-ins) to create and manage Joystick
+ * instances from a 'static' entry point.
+ * 
+ * @param idx optional plug-in wrapper index
+ * @return index by which the plug-in instance should be referred to in future calls
+ */
+Joystick.register = function(idx) {
+	if (arguments.length == 0 || idx < 0) {
+		var stick = new Joystick();
+		if (stick) {
+			return Joystick.registered.push(stick) - 1;
+		}
+	} else {
+		if (Joystick.registered[idx]) {
+			return idx;
+		}
+		var stick = new Joystick();
+		if (stick) {
+			Joystick.registered[idx] = stick;
+			return idx;
+		}
 	}
-	return Joystick.CENTER;
+	return -1;
 };
 
 /**
- * Returns the controller's main stick y-axis.
+ * Helper method for Flash (or other plug-ins) to read mutliple controller
+ * properties in one call from a static entry point. The work of the call is
+ * handled by the three parameter read().
  * 
- * @return a value between 0 and 65535 (with 0 being extreme top and 65535 extreme bottom)
+ * @param idx joystick instance index from register()
+ * @return array of controller parameters
  */
-Joystick.prototype.getY = function() {
-	if (this.ctl) {
-		return this.ctl.y;
+Joystick.read = function(idx /*followed by variable args*/) {
+	var stick = Joystick.registered[idx];
+	if (stick) {
+		return stick.read(arguments, 1, arguments.length - 1);
 	}
-	return Joystick.CENTER;
-};
-
-/**
- * Reads the state of all the devices's non-directional buttons.
- * 
- * @return the buttons represented as bits in an integer
- */
-Joystick.prototype.getButtons = function() {
-	if (this.ctl) {
-		return this.ctl.buttons;
-	}
-	return 0;
-};
+	return null;
+}
